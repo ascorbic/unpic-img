21225c254f0ed9b450152f1293313d8883e39318
Matt Kane <m@mk.gg>
2023-02-06 18:30:58 +0000

    Add auto-background

--- examples/react/App.tsx
+++ examples/react/App.tsx
@@ -6,8 +6,10 @@ export function App() {
       <Image
         src="https://images.unsplash.com/photo-1674255909399-9bcb2cab6489?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=996&q=80"
         layout="fullWidth"
-        alt="Unsplash"
+        alt=""
+        priority
         height={400}
+        background="auto"
       />
       <Image
         src="https://cdn.shopify.com/static/sample-images/bath_grande_crop_center.jpeg"
--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -1,4 +1,3 @@
-import type { JSX } from "@builder.io/mitosis/jsx-runtime";
 import {
   getTransformerForCdn,
   getTransformerForUrl,
@@ -19,163 +18,6 @@ export interface ImageSourceOptions {
   cdn?: ImageCdn;
 }
 
-export const getSizes = (
-  width?: number,
-  layout?: Layout
-): string | undefined => {
-  if (!width || !layout) {
-    return undefined;
-  }
-  switch (layout) {
-    // If screen is wider than the max size, image width is the max size,
-    // otherwise it's the width of the screen
-    case `constrained`:
-      return `(min-width: ${width}px) ${width}px, 100vw`;
-
-    // Image is always the same width, whatever the size of the screen
-    case `fixed`:
-      return `${width}px`;
-
-    // Image is always the width of the screen
-    case `fullWidth`:
-      return `100vw`;
-
-    default:
-      return undefined;
-  }
-};
-
-const removeUndefined = (obj: Record<string, string | undefined>) =>
-  Object.entries(obj).reduce((acc, [key, value]) => {
-    if (value) {
-      acc[key] = value;
-    }
-    return acc;
-  }, {} as Record<string, string>);
-
-const pixelate = (value?: number) =>
-  value || value === 0 ? `${value}px` : undefined;
-export const getStyle = <TImageAttributes extends CoreImageAttributes>({
-  width,
-  height,
-  aspectRatio,
-  layout,
-  objectFit = "cover",
-}: Pick<
-  UnpicImageProps<TImageAttributes>,
-  "width" | "height" | "aspectRatio" | "layout" | "objectFit"
->): TImageAttributes["style"] => {
-  if (layout === "fixed") {
-    return removeUndefined({
-      objectFit,
-      width: pixelate(width),
-      height: pixelate(height),
-    });
-  }
-  if (layout === "constrained") {
-    return removeUndefined({
-      objectFit,
-      maxWidth: pixelate(width),
-      maxHeight: pixelate(height),
-      aspectRatio: aspectRatio ? `${aspectRatio}` : undefined,
-      width: "100%",
-    });
-  }
-  if (layout === "fullWidth") {
-    return removeUndefined({
-      objectFit,
-      width: "100%",
-      aspectRatio: aspectRatio ? `${aspectRatio}` : undefined,
-      height: pixelate(height),
-    });
-  }
-  return {};
-};
-
-const DEFAULT_RESOLUTIONS = [
-  6016, // 6K
-  5120, // 5K
-  4480, // 4.5K
-  3840, // 4K
-  3200, // QHD+
-  2560, // WQXGA
-  2048, // QXGA
-  1920, // 1080p
-  1668, // Various iPads
-  1280, // 720p
-  1080, // iPhone 6-8 Plus
-  960, // older horizontal phones
-  828, // iPhone XR/11
-  750, // iPhone 6-8
-  640, // older and lower-end phones
-];
-
-export const getBreakpoints = ({
-  width,
-  layout,
-}: {
-  width?: number;
-  layout: Layout;
-}): number[] => {
-  if (layout === "fullWidth") {
-    return DEFAULT_RESOLUTIONS;
-  }
-  if (!width) {
-    return [];
-  }
-  const doubleWidth = width * 2;
-  if (layout === "fixed") {
-    return [width, doubleWidth];
-  }
-  if (layout === "constrained") {
-    return [
-      width,
-      doubleWidth,
-      // Filter out any resolutions that are larger than the double-res image
-      ...DEFAULT_RESOLUTIONS.filter((w) => w < doubleWidth),
-    ];
-  }
-
-  return [];
-};
-
-export const getSrcSet = ({
-  src,
-  width,
-  layout,
-  height,
-  aspectRatio,
-  breakpoints,
-  cdn,
-  transformer,
-}: ImageSourceOptions): string | undefined => {
-  transformer ||= cdn ? getTransformerForCdn(cdn) : getTransformerForUrl(src);
-
-  if (!transformer) {
-    return;
-  }
-  breakpoints ||= getBreakpoints({ width, layout });
-  return breakpoints
-    .map((bp) => {
-      let transformedHeight;
-      if (height && aspectRatio) {
-        transformedHeight = Math.round(bp * aspectRatio);
-      }
-      // Not sure why TS isn't narrowing the type here
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const transformed = transformer!({
-        url: src,
-        width: bp,
-        height: transformedHeight,
-      });
-      if (transformed) {
-        return `${transformed.toString()} ${bp}w`;
-      }
-      return "";
-    })
-    .join(",\n");
-};
-
 /**
  * HTML image attributes, common to the different JSX image components
  */
@@ -187,6 +29,7 @@ export interface CoreImageAttributes<TCSS = Record<string, string>> {
   loading?: "eager" | "lazy";
   decoding?: "sync" | "async" | "auto";
   style?: TCSS;
+  srcSet?: string;
   srcset?: string;
   role?: string;
   sizes?: string;
@@ -195,10 +38,11 @@ export interface CoreImageAttributes<TCSS = Record<string, string>> {
 }
 
 export type BaseImageProps<TImageAttributes extends CoreImageAttributes> =
-  Exclude<TImageAttributes, "srcset" | "style"> &
+  Exclude<TImageAttributes, "srcset" | "style" | "srcSet"> &
     ImageSourceOptions & {
       priority?: boolean;
       fetchpriority?: "high" | "low";
+      background?: string;
       objectFit?:
         | "contain"
         | "cover"
@@ -261,6 +105,198 @@ export type UnpicImageProps<TImageAttributes extends CoreImageAttributes> =
   | ConstrainedImageProps<TImageAttributes>
   | FullWidthImageProps<TImageAttributes>;
 
+/**
+ * Gets the `sizes` attribute for an image, based on the layout and width
+ */
+export const getSizes = (
+  width?: number,
+  layout?: Layout
+): string | undefined => {
+  if (!width || !layout) {
+    return undefined;
+  }
+  switch (layout) {
+    // If screen is wider than the max size, image width is the max size,
+    // otherwise it's the width of the screen
+    case `constrained`:
+      return `(min-width: ${width}px) ${width}px, 100vw`;
+
+    // Image is always the same width, whatever the size of the screen
+    case `fixed`:
+      return `${width}px`;
+
+    // Image is always the width of the screen
+    case `fullWidth`:
+      return `100vw`;
+
+    default:
+      return undefined;
+  }
+};
+
+const removeUndefined = (obj: Record<string, string | undefined>) =>
+  Object.entries(obj).reduce((acc, [key, value]) => {
+    if (value) {
+      acc[key] = value;
+    }
+    return acc;
+  }, {} as Record<string, string>);
+
+const pixelate = (value?: number) =>
+  value || value === 0 ? `${value}px` : undefined;
+
+/**
+ * Gets the styles for an image
+ */
+export const getStyle = <TImageAttributes extends CoreImageAttributes>({
+  width,
+  height,
+  aspectRatio,
+  layout,
+  objectFit = "cover",
+  background,
+}: Pick<
+  UnpicImageProps<TImageAttributes>,
+  "width" | "height" | "aspectRatio" | "layout" | "objectFit" | "background"
+>): TImageAttributes["style"] => {
+  const imageBackground: TImageAttributes["style"] = {};
+
+  // If background is a URL, set it to cover the image and not repeat
+  if (background?.startsWith("https://") || background?.startsWith("http://")) {
+    imageBackground.background = `url(${background})`;
+    imageBackground.backgroundSize = "cover";
+    imageBackground.backgroundRepeat = "no-repeat";
+  }
+
+  if (layout === "fixed") {
+    return removeUndefined({
+      background,
+      ...imageBackground,
+      objectFit,
+      width: pixelate(width),
+      height: pixelate(height),
+    });
+  }
+  if (layout === "constrained") {
+    return removeUndefined({
+      background,
+      ...imageBackground,
+      objectFit,
+      maxWidth: pixelate(width),
+      maxHeight: pixelate(height),
+      aspectRatio: aspectRatio ? `${aspectRatio}` : undefined,
+      width: "100%",
+    });
+  }
+  if (layout === "fullWidth") {
+    return removeUndefined({
+      background,
+      ...imageBackground,
+      objectFit,
+      width: "100%",
+      aspectRatio: aspectRatio ? `${aspectRatio}` : undefined,
+      height: pixelate(height),
+    });
+  }
+  return {};
+};
+
+const DEFAULT_RESOLUTIONS = [
+  6016, // 6K
+  5120, // 5K
+  4480, // 4.5K
+  3840, // 4K
+  3200, // QHD+
+  2560, // WQXGA
+  2048, // QXGA
+  1920, // 1080p
+  1668, // Various iPads
+  1280, // 720p
+  1080, // iPhone 6-8 Plus
+  960, // older horizontal phones
+  828, // iPhone XR/11
+  750, // iPhone 6-8
+  640, // older and lower-end phones
+];
+
+// Width of the blurred, low-res image
+const LOW_RES_WIDTH = 20;
+
+/**
+ * Gets the breakpoints for an image, based on the layout and width
+ */
+export const getBreakpoints = ({
+  width,
+  layout,
+}: {
+  width?: number;
+  layout: Layout;
+}): number[] => {
+  if (layout === "fullWidth") {
+    return DEFAULT_RESOLUTIONS;
+  }
+  if (!width) {
+    return [];
+  }
+  const doubleWidth = width * 2;
+  if (layout === "fixed") {
+    return [width, doubleWidth];
+  }
+  if (layout === "constrained") {
+    return [
+      width,
+      doubleWidth,
+      // Filter out any resolutions that are larger than the double-res image
+      ...DEFAULT_RESOLUTIONS.filter((w) => w < doubleWidth),
+    ];
+  }
+
+  return [];
+};
+
+/**
+ * Generate an image srcset
+ */
+export const getSrcSet = ({
+  src,
+  width,
+  layout,
+  height,
+  aspectRatio,
+  breakpoints,
+  cdn,
+  transformer,
+}: ImageSourceOptions): string | undefined => {
+  transformer ||= cdn ? getTransformerForCdn(cdn) : getTransformerForUrl(src);
+
+  if (!transformer) {
+    return;
+  }
+  breakpoints ||= getBreakpoints({ width, layout });
+  return breakpoints
+    .map((bp) => {
+      let transformedHeight;
+      if (height && aspectRatio) {
+        transformedHeight = Math.round(bp * aspectRatio);
+      }
+      // Not sure why TS isn't narrowing the type here
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const transformed = transformer!({
+        url: src,
+        width: bp,
+        height: transformedHeight,
+      });
+      if (transformed) {
+        return `${transformed.toString()} ${bp}w`;
+      }
+      return "";
+    })
+    .join(",\n");
+};
+
+/**
+ * Generates the props for an img element
+ */
 export function transformProps<TImageAttributes extends CoreImageAttributes>({
   src,
   width,
@@ -271,22 +307,28 @@ export function transformProps<TImageAttributes extends CoreImageAttributes>({
   cdn,
   transformer,
   objectFit = "cover",
+  background,
   ...props
 }: UnpicImageProps<TImageAttributes>): TImageAttributes {
   transformer ||= cdn ? getTransformerForCdn(cdn) : getTransformerForUrl(src);
 
+  // High priority images should be loaded eagerly
   if (priority) {
     props.loading ||= "eager";
     props.fetchpriority ||= "high";
   } else {
+    // Otherwise use native lazy loading and async decoding
     props.loading ||= "lazy";
     props.decoding ||= "async";
   }
 
+  // If the user has set the alt attribute to an empty string, we assume the
+  // image is decorative and set the role to presentation
   if (props.alt === "") {
     props.role ||= "presentation";
   }
 
+  // Calculate dimensions from aspect ratio
   if (aspectRatio) {
     if (width) {
       if (height) {
@@ -305,8 +347,24 @@ export function transformProps<TImageAttributes extends CoreImageAttributes>({
     }
   } else if (width && height) {
     aspectRatio = width / height;
-  } else {
+  } else if (layout !== "fullWidth") {
+    // Fullwidth images don't need dimensions
     console.error("Either aspectRatio or both width and height must be set");
+  }
+
+  // Auto-generate a low-res image for blurred placeholders
+  if (transformer && background === "auto") {
+    const lowResHeight = aspectRatio
+      ? Math.round(LOW_RES_WIDTH * aspectRatio)
+      : undefined;
+    const lowResImage = transformer({
+      url: src,
+      width: LOW_RES_WIDTH,
+      height: lowResHeight,
+    });
+    if (lowResImage) {
+      background = lowResImage.toString();
+    }
   }
 
   const styleProps = {
@@ -315,9 +373,10 @@ export function transformProps<TImageAttributes extends CoreImageAttributes>({
     aspectRatio,
     layout,
     objectFit,
+    background,
   } as Pick<
     UnpicImageProps<TImageAttributes>,
-    "width" | "height" | "aspectRatio" | "layout" | "objectFit"
+    "width" | "height" | "aspectRatio" | "layout" | "objectFit" | "background"
   >;
 
   if (transformer) {
@@ -331,8 +390,8 @@ export function transformProps<TImageAttributes extends CoreImageAttributes>({
     if (transformed) {
       src = transformed.toString();
     }
-
-    props.srcset ||= getSrcSet({
+    // Different JSX implementations have different casing for srcset
+    props.srcset = props.srcSet = getSrcSet({
       src,
       width,
       height,
