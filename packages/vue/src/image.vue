a4e5466055141ca05eea7eaa7c5ec26bbca44184
Matt Kane <m@mk.gg>
2023-02-08 21:21:40 +0000

    Add Vue

---
+++ packages/vue/src/image.vue
@@ -0,0 +1,42 @@
+<template>
+  <img v-bind="imageProps" />
+</template>
+
+<script lang="ts">
+import {
+  CoreImageAttributes,
+  transformProps,
+  UnpicImageProps,
+  getStyle,
+} from "@unpic/core";
+
+export default {
+  name: "image",
+  props: {
+    layout: {
+      type: String,
+      required: false,
+    },
+    background: {
+      type: String,
+      required: false,
+    },
+    priority: {
+      type: Boolean,
+      required: false,
+    },
+    cdn: {
+      type: String,
+      required: false,
+    },
+  },
+  computed: {
+    imageProps() {
+      return transformProps({
+        ...this.$props,
+        ...this.$attrs,
+      } as UnpicImageProps<CoreImageAttributes>);
+    },
+  },
+};
+</script>
---
+++ packages/vue/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "Image",
+      fileName: "image",
+    },
+    rollupOptions: {
+      external: ["vue", "@unpic/core"],
+    },
+  },
+});
