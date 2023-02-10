6df68f1f61361d529b1faf19f053d860ace10be8
Matt Kane <m@mk.gg>
2023-02-10 06:46:38 +0000

    wip: webc

--- packages/solid/tsup.config.ts
+++ packages/solid/tsup.config.ts
@@ -3,12 +3,10 @@ import { defineConfig } from "tsup-preset-solid";
 export default defineConfig(
   {
     entry: "src/index.ts",
-    devEntry: true,
   },
   {
     // Enable this to write export conditions to package.json
     writePackageJson: true,
     dropConsole: true,
-    cjs: true,
   }
 );
