66df2e80dc7ffd019ae31af8e8ab4c963e65304a
Elad Rosenheim <eladroz@users.noreply.github.com>
2023-02-21 14:16:46 +0200

    fix(react): camelize props, allow passing ref (#20)
    Co-authored-by: Matt Kane <m@mk.gg>

---
+++ packages/react/src/camelize.ts
@@ -0,0 +1,18 @@
+import type { HTMLAttributes } from "react";
+
+const camelize = (s: string) =>
+  s.replace(/-./g, (substr) => substr[1].toUpperCase());
+
+const nestedKeys = new Set(["style"]);
+const fixedMap: Record<string, string> = { srcset: "srcSet" };
+
+export function camelizeProps<TObject extends HTMLAttributes<HTMLElement>>(
+  props: TObject
+): TObject {
+  return Object.fromEntries(
+    Object.entries(props).map(([k, v]) => [
+      fixedMap[k] || camelize(k),
+      nestedKeys.has(k) && v && typeof v !== "string" ? camelizeProps(v) : v,
+    ])
+  ) as TObject;
+}
