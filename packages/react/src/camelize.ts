898e950007d4f93ef94fbabe89418bdc871bb58a
Matt Kane <m@mk.gg>
2023-02-21 12:02:21 +0000

    fix: update types for camelize

--- packages/react/src/camelize.ts
+++ packages/react/src/camelize.ts
@@ -1,13 +1,18 @@
-export function camelizeProps(o: object): object {
-  const fixedMap: Record<string, string> = { srcset: "srcSet" };
-  const nestedKeys = ["style"];
-  const camelize = (s: string) =>
-    s.replace(/-./g, (substr) => substr[1].toUpperCase());
+import type { HTMLAttributes } from "react";
 
+const camelize = (s: string) =>
+  s.replace(/-./g, (substr) => substr[1].toUpperCase());
+
+const nestedKeys = new Set(["style"]);
+const fixedMap: Record<string, string> = { srcset: "srcSet" };
+
+export function camelizeProps<TObject extends HTMLAttributes<HTMLElement>>(
+  props: TObject
+): TObject {
   return Object.fromEntries(
-    Object.entries(o).map(([k, v]) => [
+    Object.entries(props).map(([k, v]) => [
       fixedMap[k] || camelize(k),
-      nestedKeys.includes(k) && v ? camelizeProps(v) : v,
+      nestedKeys.has(k) && v && typeof v !== "string" ? camelizeProps(v) : v,
     ])
-  );
+  ) as TObject;
 }
