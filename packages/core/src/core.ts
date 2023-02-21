9033704c6f6df7b50fc5beed0338defaa50b9a4c
Daniel Esteves <me+github@danestves.com>
2023-02-21 10:26:47 -0400

    fix: pass `transformer` to srcset generate function

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -426,6 +426,7 @@ export function transformProps<
       aspectRatio,
       layout,
       breakpoints,
+      transformer,
     });
 
     if (layout === "fullWidth" || layout === "constrained") {
