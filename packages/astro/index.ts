eed3472b11bacaed558a6edacaac466123823ae5
Leander Gilles <leander.gilles@pm.me>
2023-02-21 12:29:15 +0100

    feat(astro): add Astro support (#16)
    Co-authored-by: Leander Gilles <Leander@neugelb.com>
    Co-authored-by: Matt Kane <m@mk.gg>

---
+++ packages/astro/index.ts
@@ -0,0 +1,4 @@
+// Do not write code directly here, instead use the `src` folder!
+// Then, use this file to export everything you want your user to access.
+
+export { default as Image } from "./src/Image.astro";
