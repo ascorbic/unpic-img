d3047236bae2728585bed42a866cbbe22c820b52
Matt Kane <m@mk.gg>
2023-03-19 20:00:45 +0000

    docs: add sitemap (#137)

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    // Enable Preact to support Preact JSX components.
-    preact(),
-    // Enable React for the Algolia search component.
-    react(),
-  ],
-  site: `https://unpic.pics`,
+  // Enable Preact to support Preact JSX components.
+  preact(),
+  // Enable React for the Algolia search component.
+  react(), sitemap()],
+  site: `https://unpic.pics`
 });
