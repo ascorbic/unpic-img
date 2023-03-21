396e0581c28134d04de4e9f624e53b725d6c2f91
Matt Kane <m@mk.gg>
2023-03-21 16:13:49 +0000

    fix(angular): handle input changes (#157)

--- examples/angular/src/app/app.component.ts
+++ examples/angular/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular';
+  imageForm = new FormGroup({
+    width: new FormControl(),
+    height: new FormControl(),
+  });
+
+  get width() {
+    return this.imageForm.get('width')?.value || 800;
+  }
+
+  get height() {
+    return this.imageForm.get('height')?.value || 600;
+  }
 }
