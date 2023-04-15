68729a8236fed59baec62205e93b4847fba0e673
David Di Biase <1168397+davedbase@users.noreply.github.com>
2023-04-15 11:52:48 -0400

    Patching build proc to retain JSX and support CJS

--- packages/solid/tsup.config.ts
+++ packages/solid/tsup.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "tsup-preset-solid";
 
 export default defineConfig(
   {
-    entry: "src/index.ts",
+    entry: "src/index.tsx",
   },
   {
     // Enable this to write export conditions to package.json
     writePackageJson: true,
     dropConsole: true,
+    cjs: true
   }
 );
