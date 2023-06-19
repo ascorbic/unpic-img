089245b7e5fd5f744040993235a86d45b4cf4319
Matt Kane <m@mk.gg>
2023-06-19 16:23:30 +0100

    fix(qwik): don't externalise `@unpic/core` dep (#254)
    I'm not sure why, but when installed with pnpm, rollup is unable to resolve the `@unpic/core` transitive dependency. This PR moves the package out of external deps, so that it is compiled into the bundle. Fixes #69

--- packages/qwik/vite.config.ts
+++ packages/qwik/vite.config.ts
@@ -10,9 +10,6 @@ export default defineConfig(() => {
         formats: ["es", "cjs"],
         fileName: (format) => `index.qwik.${format === "es" ? "mjs" : "cjs"}`,
       },
-      rollupOptions: {
-        external: ["@unpic/core"],
-      },
     },
     plugins: [qwikVite()],
   };
