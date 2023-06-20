732bc61c7448dca663ced57127cbbbfa07381008
Matt Kane <m@mk.gg>
2023-06-20 08:24:23 +0100

    chore: update all deps

--- packages/qwik/test/qwik.test.tsx
+++ packages/qwik/test/qwik.test.tsx
@@ -9,8 +9,7 @@ import {
 describe("the Qwik component", async () => {
   for (const props of testCases) {
     const { screen, render } = await createDOM();
-    // Skip until https://github.com/BuilderIO/qwik/pull/3135 is released
-    test.skip(`renders a ${props.layout} image`, async () => {
+    test(`renders a ${props.layout} image`, async () => {
       await render(<Image {...props} id={props.layout} />);
       const img = screen.querySelector<HTMLImageElement>(`#${props.layout}`);
 
