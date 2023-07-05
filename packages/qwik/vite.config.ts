f4f2f95faa4768a7bef703efafd9660b6fc48dab
Matt Kane <m@mk.gg>
2023-07-05 07:24:27 +0100

    chore: weird vitest hacks for qwik.

--- packages/qwik/vite.config.ts
+++ packages/qwik/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
-
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
 export default defineConfig(() => {
   return {
     build: {
@@ -9,6 +10,13 @@ export default defineConfig(() => {
         entry: "./src/index.ts",
         formats: ["es", "cjs"],
         fileName: (format) => `index.qwik.${format === "es" ? "mjs" : "cjs"}`,
+      },
+    },
+    resolve: {
+      alias: {
+        // Not sure why, but Vitest needs these for Qwik
+        "@builder.io/qwik/build": require.resolve("@builder.io/qwik/build"),
+        "/src/": new URL("./src/", import.meta.url).pathname,
       },
     },
     plugins: [qwikVite()],
