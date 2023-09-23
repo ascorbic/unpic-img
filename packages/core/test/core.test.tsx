03c0c08465f70799e6aa11701433f1f044b5d428
Matt Kane <m@mk.gg>
2023-09-23 09:27:08 +0100

    fix(core): gracefully handle missing props

--- packages/core/test/core.test.tsx
+++ packages/core/test/core.test.tsx
@@ -1,34 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { describe, test, expect } from "vitest";
-import { transformProps } from '../src/core';
+import { transformProps } from "../src/core";
 
 describe("Core", () => {
   test("updates the src URL to include transformations", () => {
     const props = transformProps({
-      src: 'https://res.cloudinary.com/example/image/upload/images/my-image',
+      src: "https://res.cloudinary.com/example/image/upload/images/my-image",
       width: 800,
       height: 600,
-      layout: 'constrained',
-      objectFit: 'cover',
+      layout: "constrained",
+      objectFit: "cover",
     });
-    expect(props.src).toEqual('https://res.cloudinary.com/example/image/upload/w_800,h_600,c_lfill,f_auto/images/my-image');
+    expect(props.src).toEqual(
+      "https://res.cloudinary.com/example/image/upload/w_800,h_600,c_lfill,f_auto/images/my-image"
+    );
   });
 
   test("allows an image ID for src when using a custom transformer", () => {
-    const id = 'images/my-image';
+    const id = "images/my-image";
     const width = 800;
     const height = 800;
     const props = transformProps({
       src: id,
       width,
       height,
-      layout: 'constrained',
-      objectFit: 'cover',
+      layout: "constrained",
+      objectFit: "cover",
       transformer: ({ url: tUrl, width: tWidth, height: tHeight }) => {
-        return `https://res.cloudinary.com/example/image/upload/w_${tWidth},h_${tHeight},c_lfill,f_auto/${tUrl}`
-      }
+        return `https://res.cloudinary.com/example/image/upload/w_${tWidth},h_${tHeight},c_lfill,f_auto/${tUrl}`;
+      },
     });
-    expect(props.src).toEqual(`https://res.cloudinary.com/example/image/upload/w_${width},h_${height},c_lfill,f_auto/${id}`);
+    expect(props.src).toEqual(
+      `https://res.cloudinary.com/example/image/upload/w_${width},h_${height},c_lfill,f_auto/${id}`
+    );
+  });
+
+  test("doesn't throw if props are empty", () => {
+    const props = transformProps({} as any);
+    expect(props).toBeDefined();
+    expect(props.src).toBeUndefined();
+    expect(props.loading).toEqual("lazy");
   });
 });
-
-
