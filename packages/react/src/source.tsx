658cbb22fdf42a144acbea639f7a8fd4a0a5803a
Matt Kane <m@mk.gg>
2023-09-30 10:43:09 +0100

    fix: camelise source props

--- packages/react/src/source.tsx
+++ packages/react/src/source.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
 import { transformSourceProps, UnpicSourceProps } from "@unpic/core";
+import { camelizeProps } from "./camelize";
 export type SourceProps = UnpicSourceProps;
 
 export const Source = React.forwardRef<HTMLSourceElement, SourceProps>(
   function Source(props, ref) {
-    return <source {...transformSourceProps(props)} ref={ref} />;
+    const camelizedProps = camelizeProps(
+      transformSourceProps<React.SourceHTMLAttributes<HTMLSourceElement>>(
+        props,
+      ),
+    );
+
+    return <source {...camelizedProps} ref={ref} />;
   },
 );
