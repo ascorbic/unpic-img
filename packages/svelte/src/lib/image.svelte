374827bb6c7c10c763fb9568bc15084b1312f2f3
Matt Kane <m@mk.gg>
2023-09-30 09:44:47 +0100

    chore: type fixes

--- packages/svelte/src/lib/image.svelte
+++ packages/svelte/src/lib/image.svelte
@@ -2,6 +2,7 @@
   import { transformProps } from "@unpic/core";
   import styleToCss from "style-object-to-css-string";
   import type { ImageProps as BaseImageProps } from "./types";
+  import type { HTMLAttributes } from "svelte/elements";
   // This unused import is a hack to get around a bug in svelte2tsx
   import type { UrlTransformer, ImageCdn } from "unpic";
 
@@ -12,8 +13,15 @@
     alt,
     style: styleObj,
     ...transformedProps
-  } = transformProps({ ...props, style: {} as Record<string, string> }));
-  $: style = [styleToCss(styleObj), parentStyle].filter(Boolean).join(";");
+  } = transformProps<
+    Omit<HTMLAttributes<HTMLImageElement>, "style"> & {
+      style: Record<string, string>;
+      alt?: string | null;
+    }
+  >({ ...props }));
+  $: style = [styleToCss(styleObj || {}), parentStyle]
+    .filter(Boolean)
+    .join(";");
 </script>
 
 <img {alt} {style} {...transformedProps} />
