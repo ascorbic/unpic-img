3a7233fba0bf8fe3c752e58a1fc1db94eed9de68
ascorbic <ascorbic@gmail.com>
2023-11-16 08:55:12 +0000

    chore: update test

--- packages/react/test/next.test.tsx
+++ packages/react/test/next.test.tsx
@@ -1,6 +1,6 @@
 import { describe, test, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { Image } from "../src/next";
+import { Image } from "../src/nextjs";
 import {
   expectImagePropsToMatchTransformed,
   imgTestCases,
