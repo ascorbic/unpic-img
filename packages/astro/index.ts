a5afc56156e4f2d11ce0a5ce6aff6acb13677743
John L. Armstrong IV <20903247+jlarmstrongiv@users.noreply.github.com>
2024-01-04 12:06:39 -0500

    fix: ignore Astro types in index.ts (#424)
    Co-authored-by: Matt Kane <m@mk.gg>

--- packages/astro/index.ts
+++ packages/astro/index.ts
@@ -1,5 +1,7 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
+// @ts-ignore
 export { default as Image } from "./src/Image.astro";
+// @ts-ignore
 export { default as Source } from "./src/Source.astro";
