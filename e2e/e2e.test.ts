228936e80a1990da78dce515e7dd422d56370393
ascorbic <ascorbic@gmail.com>
2024-02-24 11:54:15 +0000

    chore: setup playwright action

--- e2e/e2e.test.ts
+++ e2e/e2e.test.ts
@@ -1,5 +1,8 @@
 import { test, expect as baseExpect } from "@playwright/test";
 import type { Locator } from "@playwright/test";
+
+export const site = process.env.SITE ?? "preact";
+
 export const expect = baseExpect.extend({
   async toHaveLoadedImage(locator: Locator) {
     const assertionName = "toHaveLoadedImage";
@@ -20,81 +23,80 @@ export const expect = baseExpect.extend({
   },
 });
 
-const base =
-  process.env.BASE_URL ||
-  process.env.DEPLOY_PRIME_URL ||
-  "http://localhost:8888";
+const base = "http://localhost:8888";
 
 function aspectRatio(width: number = 0, aspectRatio: number) {
   return Math.round(width / aspectRatio);
 }
 
-test("desktop", async ({ page }) => {
-  await page.goto(`${base}/test`);
-  const fw = await page.getByAltText("fullWidth");
-  expect(fw).toBeVisible();
-  const vp = await page.viewportSize();
-  await expect(fw).toHaveLoadedImage();
-  await expect(fw).toHaveJSProperty("width", vp?.width);
-  await expect(fw).toHaveJSProperty("height", aspectRatio(vp?.width, 16 / 9));
+test.describe(site, () => {
+  test("desktop", async ({ page }) => {
+    await page.goto(`${base}/test`);
+    const fw = await page.getByAltText("fullWidth");
+    expect(fw).toBeVisible();
+    const vp = await page.viewportSize();
+    await expect(fw).toHaveLoadedImage();
+    await expect(fw).toHaveJSProperty("width", vp?.width);
+    await expect(fw).toHaveJSProperty("height", aspectRatio(vp?.width, 16 / 9));
 
-  const cs = await page.getByAltText("constrained");
-  await cs.scrollIntoViewIfNeeded();
-  expect(cs).toBeVisible();
-  await expect(cs).toHaveLoadedImage();
-  await expect(cs).toHaveJSProperty("width", 800);
-  await expect(cs).toHaveJSProperty("height", 600);
+    const cs = await page.getByAltText("constrained");
+    await cs.scrollIntoViewIfNeeded();
+    expect(cs).toBeVisible();
+    await expect(cs).toHaveLoadedImage();
+    await expect(cs).toHaveJSProperty("width", 800);
+    await expect(cs).toHaveJSProperty("height", 600);
 
-  const fx = await page.getByAltText("fixed");
-  expect(fx).toBeVisible();
-  await expect(fx).toHaveLoadedImage();
-  await expect(fx).toHaveJSProperty("width", 800);
-  await expect(fx).toHaveJSProperty("height", 600);
+    const fx = await page.getByAltText("fixed");
+    expect(fx).toBeVisible();
+    await expect(fx).toHaveLoadedImage();
+    await expect(fx).toHaveJSProperty("width", 800);
+    await expect(fx).toHaveJSProperty("height", 600);
 
-  const os = await page.getByAltText("offscreen");
+    const os = await page.getByAltText("offscreen");
 
-  await expect(os).not.toBeInViewport();
-  await expect(os).toHaveJSProperty("width", 600);
-  await expect(os).toHaveJSProperty("height", 800);
-  await expect(os).not.toHaveLoadedImage();
-  await os.scrollIntoViewIfNeeded();
-  await expect(os).toBeInViewport();
-  await expect(os).toHaveLoadedImage();
-  await expect(os).toHaveJSProperty("width", 600);
-  await expect(os).toHaveJSProperty("height", 800);
-});
+    await expect(os).not.toBeInViewport();
+    await expect(os).toHaveJSProperty("width", 600);
+    await expect(os).toHaveJSProperty("height", 800);
+    await expect(os).not.toHaveLoadedImage();
+    await os.scrollIntoViewIfNeeded();
+    await expect(os).toBeInViewport();
+    await expect(os).toHaveLoadedImage();
+    await expect(os).toHaveJSProperty("width", 600);
+    await expect(os).toHaveJSProperty("height", 800);
+  });
 
-test("mobile", async ({ page }) => {
-  await page.setViewportSize({ width: 400, height: 800 });
-  await page.goto(`${base}/test`);
-  const fw = await page.getByAltText("fullWidth");
-  expect(fw).toBeVisible();
-  const vp = await page.viewportSize();
-  await expect(fw).toHaveLoadedImage();
-  await expect(fw).toHaveJSProperty("width", vp?.width);
-  await expect(fw).toHaveJSProperty("height", aspectRatio(vp?.width, 16 / 9));
+  test("mobile", async ({ page }) => {
+    await page.setViewportSize({ width: 400, height: 800 });
+    await page.goto(`${base}/test`);
+    const fw = await page.getByAltText("fullWidth");
+    expect(fw).toBeVisible();
+    const vp = await page.viewportSize();
+    await expect(fw).toHaveLoadedImage();
+    await expect(fw).toHaveJSProperty("width", vp?.width);
+    await expect(fw).toHaveJSProperty("height", aspectRatio(vp?.width, 16 / 9));
 
-  const cs = await page.getByAltText("constrained");
-  await cs.scrollIntoViewIfNeeded();
-  expect(cs).toBeVisible();
-  await expect(cs).toHaveLoadedImage();
-  await expect(cs).toHaveJSProperty("width", 400);
-  await expect(cs).toHaveJSProperty("height", 300);
+    const cs = await page.getByAltText("constrained");
+    await cs.scrollIntoViewIfNeeded();
+    expect(cs).toBeVisible();
+    await expect(cs).toHaveLoadedImage();
+    await expect(cs).toHaveJSProperty("width", 400);
+    await expect(cs).toHaveJSProperty("height", 300);
 
-  const fx = await page.getByAltText("fixed");
-  expect(fx).toBeVisible();
-  await expect(fx).toHaveLoadedImage();
-  await expect(fx).toHaveJSProperty("width", 800);
-  await expect(fx).toHaveJSProperty("height", 600);
+    const fx = await page.getByAltText("fixed");
+    expect(fx).toBeVisible();
+    await expect(fx).toHaveLoadedImage();
+    await expect(fx).toHaveJSProperty("width", 800);
+    await expect(fx).toHaveJSProperty("height", 600);
 
-  const os = await page.getByAltText("offscreen");
-  await expect(os).not.toBeInViewport();
-  await expect(os).toHaveJSProperty("width", 400);
-  await expect(os).toHaveJSProperty("height", aspectRatio(400, 3 / 4));
-  await expect(os).not.toHaveLoadedImage();
-  await os.scrollIntoViewIfNeeded();
-  await expect(os).toBeInViewport();
-  await expect(os).toHaveLoadedImage();
-  await expect(os).toHaveJSProperty("width", 400);
-  await expect(os).toHaveJSProperty("height", aspectRatio(400, 3 / 4));
+    const os = await page.getByAltText("offscreen");
+    await expect(os).not.toBeInViewport();
+    await expect(os).toHaveJSProperty("width", 400);
+    await expect(os).toHaveJSProperty("height", aspectRatio(400, 3 / 4));
+    await expect(os).not.toHaveLoadedImage();
+    await os.scrollIntoViewIfNeeded();
+    await expect(os).toBeInViewport();
+    await expect(os).toHaveLoadedImage();
+    await expect(os).toHaveJSProperty("width", 400);
+    await expect(os).toHaveJSProperty("height", aspectRatio(400, 3 / 4));
+  });
 });
---
+++ playwright.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "@playwright/test";
+
+const site = process.env.SITE ?? "preact";
+
+export default defineConfig({
+  // Look for test files in the "tests" directory, relative to this configuration file.
+  testDir: "e2e",
+  // Fail the build on CI if you accidentally left test.only in the source code.
+  forbidOnly: !!process.env.CI,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: process.env.CI ? "blob" : "html",
+  webServer: {
+    command: `netlify serve --filter examples/${site}`,
+    url: "http://127.0.0.1:8888",
+    reuseExistingServer: !process.env.CI,
+    stdout: "pipe",
+    stderr: "pipe",
+  },
+});
