db0fae07763d76a89ae0365dff760c37b01d4a3e
ascorbic <ascorbic@gmail.com>
2024-02-24 12:42:08 +0000

    chore: report types

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,9 @@ export default defineConfig({
   // Fail the build on CI if you accidentally left test.only in the source code.
   forbidOnly: !!process.env.CI,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? "blob" : "html",
+  reporter: process.env.CI
+    ? [["github"], ["blob"], ["list"]]
+    : [["html"], ["list"]],
   webServer: {
     command: `netlify serve --filter examples/${site}`,
     url: "http://127.0.0.1:8888",
