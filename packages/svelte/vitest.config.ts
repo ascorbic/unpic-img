3a7cd66d3dc4174329826345400cb426dc680510
Bert Bengtson <RBengtson@ashleyfurniture.com>
2024-12-13 09:20:50 -0500

    revert vitest Config changes

--- packages/svelte/vitest.config.ts
+++ packages/svelte/vitest.config.ts
@@ -1,13 +1,14 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { svelteTesting } from "@testing-library/svelte/vite";
 import { defineConfig } from "vite";
-import { svelteTesting } from "@testing-library/svelte/vite";
+
 export default defineConfig({
   plugins: [
     sveltekit(),
     svelteTesting({ resolveBrowser: true, autoCleanup: true }),
   ],
   test: {
+    globals: true,
     environment: "jsdom",
   },
 });
