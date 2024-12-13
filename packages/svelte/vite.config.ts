6b20358babb6b92cd0ec4afa3b12b614cb0ffbc3
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-12-12 18:39:52 -0800

    move to vitest config file

--- packages/svelte/vite.config.ts
+++ packages/svelte/vite.config.ts
@@ -3,5 +3,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
-  resolve: process.env.VITEST ? { conditions: ["browser"] } : undefined,
 });
--- packages/svelte/vitest.config.ts
+++ packages/svelte/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
+  resolve: { conditions: ["browser"] },
   test: {
     globals: true,
     environment: "jsdom",
